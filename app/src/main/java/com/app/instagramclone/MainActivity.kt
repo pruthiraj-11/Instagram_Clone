3b445edfda235ff934e3255cc7fb7d78f134480e
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2024-03-24 12:51:12 +0530

    ICSD

--- app/src/main/java/com/app/instagramclone/MainActivity.kt
+++ app/src/main/java/com/app/instagramclone/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.app.instagramclone
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler(Looper.getMainLooper()).postDelayed({
+             startActivity(Intent(this,SignupActivity::class.java))
+        },3000)
     }
 }
--- app/src/main/java/com/app/instagramclone/SignupActivity.kt
+++ app/src/main/java/com/app/instagramclone/SignupActivity.kt
@@ -1,6 +1,8 @@
 package com.app.instagramclone
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
