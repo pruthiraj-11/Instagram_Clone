b2bf52cbbc5b35e868ed5c52a1d4604cf9800859
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2024-04-04 21:07:23 +0530

    ICSD

---
+++ app/src/main/java/com/app/instagramclone/Models/User.kt
@@ -0,0 +1,20 @@
+package com.app.instagramclone.Models
+
+class User{
+    var profileimage:String?=null
+    var name:String?=null
+    var email:String?=null
+    var password:String?=null
+    constructor()
+    constructor(profileimage:String?,name:String?,email:String?,password:String?){
+        this.profileimage=profileimage
+        this.name=name
+        this.email=email
+        this.password=password
+    }
+    constructor(name:String?,email:String?,password:String?){
+        this.name=name
+        this.email=email
+        this.password=password
+    }
+}
