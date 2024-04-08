e14afffaccb3cefc244fe4923038101825190288
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2024-04-08 23:17:20 +0530

    ICSD

--- app/src/main/java/com/app/instagramclone/Models/Status.kt
+++ app/src/main/java/com/app/instagramclone/Models/Status.kt
@@ -4,15 +4,5 @@ class Status{
     var profileimage:String?=null
     
     constructor()
-    constructor(profileimage:String?,name:String?,email:String?,password:String?){
-        this.profileimage=profileimage
-        this.name=name
-        this.email=email
-        this.password=password
-    }
-    constructor(name:String?,email:String?,password:String?){
-        this.name=name
-        this.email=email
-        this.password=password
-    }
+
 }
