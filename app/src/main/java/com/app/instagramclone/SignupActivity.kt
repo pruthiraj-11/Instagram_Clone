adc836bbc5d1818a8b9a9d459d91c85a00a970a1
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2024-04-12 19:38:37 +0530

    ICSD

--- app/src/main/java/com/app/instagramclone/SignupActivity.kt
+++ app/src/main/java/com/app/instagramclone/SignupActivity.kt
@@ -16,7 +16,7 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
