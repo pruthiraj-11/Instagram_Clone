8ebc5207b13a96ddf6b4d0f252168728ecd4026b
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2024-04-13 22:00:27 +0530

    ICSD

--- app/src/main/java/com/app/instagramclone/SignInActivity.kt
+++ app/src/main/java/com/app/instagramclone/SignInActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.app.instagramclone.databinding.ActivitySignInBinding
+import com.app.instagramclone.databinding.ActivitySignupBinding
 
 class SignInActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivitySignInBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_in)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
