d257a6e913b1a55e0bdfdc335d0bb09cb08eccbb
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2024-04-20 14:07:25 +0530

    ICSD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.firebase.storage)
+    implementation(libs.shimmer)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
