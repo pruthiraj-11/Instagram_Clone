b57216ce96957771e5a06765840359615c0c42e2
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2024-05-07 20:27:09 +0530

    ICSD

--- app/src/main/java/com/app/instagramclone/ui/activities/PostActivity.kt
+++ app/src/main/java/com/app/instagramclone/ui/activities/PostActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.app.instagramclone.R
+import com.app.instagramclone.databinding.ActivityPostBinding
 
 class PostActivity : AppCompatActivity() {
+    val binding by lazy{
+        ActivityPostBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_post)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
