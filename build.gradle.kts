a810a898b568c256138124e5dea3e28babded51d
Pruthiraj Routaray <113031912+pruthiraj-11@users.noreply.github.com>
2024-05-11 20:17:43 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("com.google.dagger.hilt.android") version "2.44" apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
